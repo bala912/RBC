02af33fe3127b3d049ba564a5c1f3dacee8209cc
Bala <venkatesh.balaji09@gmail.com>
2023-05-04 01:02:10 -0400

    Spring boot skeleton setup

---
+++ gotrain/src/main/java/com/rbc/gotrain/GotrainApplication.java
@@ -0,0 +1,13 @@
+package com.rbc.gotrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotrainApplication.class, args);
+	}
+
+}
